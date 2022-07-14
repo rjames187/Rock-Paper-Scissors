1186acd223814ca8af5e46f1f87552893c507dfd
Rory <rory.james2021@gmail.com>
2022-07-14 17:40:03 -0500

    write playRound function

--- script.js
+++ script.js
@@ -11,5 +11,62 @@ function computerPlay() {
     return PLAYS[randNum];
 }
 
-console.log(computerPlay());
+// Input: 
+//      playerSelection (str) : 'rock', 'paper', or 'scissors'
+//      computerSelection (str) : 'rock', 'paper', or 'scissors'
+// Output: a string declaring the outcome of the round
+
+function playRound(playerSelection, computerSelection) {
+    // make input strings case insensitive
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    // case: tie
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+
+    // cases for player plays rock
+    if (playerSelection === 'rock') {
+        switch (computerSelection) {
+            case 'paper':
+                return "You lose! Paper beats rock!";
+                break;
+            
+            case 'scissors':
+                return "You win! Rock beats scissors!";
+        }
+    }
+
+    // cases for player plays paper
+    if (playerSelection === 'paper') {
+        switch (computerSelection) {
+            case 'scissors':
+                return "You lose! Scissors beats paper!";
+                break;
+            
+            case 'rock':
+                return "You win! Paper beats rock!";
+        }
+    }
+
+    // cases for player plays scissors
+    if (playerSelection === 'scissors') {
+        switch (computerSelection) {
+            case 'rock':
+                return "You lose! Rock beats scissors!";
+                break;
+            
+            case 'paper':
+                return "You win! Scissors beats paper!";
+        }
+    }
+
+    return "Error! Must enter 'rock', 'paper', or 'scissors'";
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
 
