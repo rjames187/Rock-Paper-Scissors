d5466d9963ab3fef749d741e26db34e3db3c10be
Rory <rory.james2021@gmail.com>
2022-07-14 17:20:10 -0500

    write and test computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world!");
+// Input: n/a | Output: str
+// randomly returns a string of either 'rock', 'paper', 
+// or 'scissors' (equal chance for each)
+
+function computerPlay() {
+    const PLAYS = ['rock', 'paper', 'scissors'];
+
+    // generate a random whole number from 0 to 2 inclusive
+    const randNum = Math.floor((Math.random() * 3));
+
+    return PLAYS[randNum];
+}
+
+console.log(computerPlay());
+
